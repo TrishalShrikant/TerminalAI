c60be0ef2876e309573e493e0818f77b6623942a
TrishalShrikant <trishalshrikant@gmail.com>
2024-08-13 21:44:26 +0530

    This is the first commit.

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+import google.generativeai as genai
+
+os.environ['GOOGLE_API_KEY'] = "YOUR API-KEY"
+genai.configure(api_key = os.environ['GOOGLE_API_KEY'])
+
+model = genai.GenerativeModel('gemini-pro')
+print("HELLO THIS IS TERMINAL-AI")
+question = ""
+print("ASK ME ANYTHING! OR TYPE NEVERMIND TO EXIT")
+while True:
+    print()
+    question = input("YOUR RESPONSE: ")
+    if question == "Nevermind" or question == "nevermind" or question == "NEVERMIND":
+        print()
+        print("BYE!")
+        exit()
+    response = model.generate_content(question)
+    print()
+    print(response.text)
